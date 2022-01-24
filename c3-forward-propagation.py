77ef044b651d4677fd39ea5041fd91747bb8ab65
ngocdung <dungdidom@gmail.com>
2022-01-24 23:54:18 +0900

    multiple inputs

--- c3-forward-propagation.py
+++ c3-forward-propagation.py
@@ -9,3 +9,18 @@ number_of_toes = [8.5, 9.5, 10, 9]
 input = number_of_toes[0]
 pred = neural_network(input, weight)
 print(pred)
+
+# Multiple datapoints
+weights = [0.1, 0.2, 0]
+def neural_network(input, weights):
+    pred = w_sum(input, weights)
+    return pred
+
+toes = [8.5, 9.5, 9.9, 9.0]
+wlrec = [0.65, 0.8, 0.8, 0.9]
+nfans = [1.2, 1.3, 0.5, 1.0]
+
+input = [toes[0], wlred[0], nfans[0]]
+
+pred = neural_network(input, weights)
+
